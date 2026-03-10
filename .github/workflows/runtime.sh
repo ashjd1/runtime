4316f1b2f126833e5dcf6cf9399fd37a89661e78
Ashutosh Jadhav <113992943+ashjd1@users.noreply.github.com>
2026-03-10 09:26:57 +0530

    Update runtime.sh

--- .github/workflows/runtime.sh
+++ .github/workflows/runtime.sh
@@ -163,7 +163,7 @@
     DOTNET_RUNTIME_ID=linux-x64
 
     common_args+=(/p:NoPgoOptimize=true --portablebuild "$PORTABLE_BUILD")
-    common_args+=(/p:DotNetBuildFromSource=true /p:UseRidGraph=true)
+    common_args+=(/p:DotNetBuildFromSource=true /p:UseRidGraph=true /p:EnableNETAnalyzers=false /p:RunAnalyzersDuringBuild=false)
     common_args+=(--runtimeconfiguration Debug --librariesConfiguration "$CONFIGURATION")
     # common_args+=(/p:PrimaryRuntimeFlavor=Mono --warnAsError false --subset clr+mono+libs+host+packs+libs.tests)
     common_args+=(/p:UsingToolMicrosoftNetCompilers=false  /p:DotNetBuildSourceOnly=true /p:DotNetBuildTests=true --cmakeargs -DCLR_CMAKE_USE_SYSTEM_BROTLI=true --cmakeargs -DCLR_CMAKE_USE_SYSTEM_ZLIB=true)
