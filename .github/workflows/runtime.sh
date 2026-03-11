b9639f66868bd19195da033aef164e38065aa722
Ashutosh Jadhav <113992943+ashjd1@users.noreply.github.com>
2026-03-11 09:46:43 +0530

    build

--- .github/workflows/runtime.sh
+++ .github/workflows/runtime.sh
@@ -165,7 +165,7 @@
     common_args+=(/p:NoPgoOptimize=true --portablebuild "$PORTABLE_BUILD")
     common_args+=(/p:DotNetBuildFromSource=true /p:UseRidGraph=true /p:EnableNETAnalyzers=false /p:RunAnalyzersDuringBuild=false /p:_EnableILLinkRoslynAnalyzer=false)
     common_args+=(--runtimeconfiguration Debug --librariesConfiguration "$CONFIGURATION")
-    # common_args+=(/p:PrimaryRuntimeFlavor=Mono --warnAsError false --subset clr+mono+libs+host+packs+libs.tests)
+    common_args+=(/p:PrimaryRuntimeFlavor=Mono --warnAsError false --subset clr+mono+libs+host+packs+libs.tests)
     common_args+=(/p:UsingToolMicrosoftNetCompilers=false  /p:DotNetBuildSourceOnly=true /p:DotNetBuildTests=true --cmakeargs -DCLR_CMAKE_USE_SYSTEM_BROTLI=true --cmakeargs -DCLR_CMAKE_USE_SYSTEM_ZLIB=true)
 
     BUILD_EXIT_CODE=0
